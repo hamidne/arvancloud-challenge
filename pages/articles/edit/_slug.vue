f396fcc43e0c489322fcc3b58b2cec7bf24fe7d8
HamidNE <hamidne@mail.ru>
2020-09-19 11:03:22 +0430

    add edit page for article

---
+++ pages/articles/edit/_slug.vue
@@ -0,0 +1,31 @@
+<template>
+  <b-container fluid>
+    <h1>New Article</h1>
+    <article-form v-model="form" @submit="submit" />
+  </b-container>
+</template>
+
+<script>
+import ArticleForm from '~/components/pages/articles/global/form/ArticleForm.vue'
+export default {
+  components: { ArticleForm },
+  data: () => ({
+    form: {
+      title: '',
+      description: '',
+      body: '',
+      tagList: [],
+    },
+  }),
+  methods: {
+    submit() {
+      this.$axios
+        .$post('/articles', { article: this.form })
+        .then(() => this.$router.push({ name: 'articles' }))
+    },
+  },
+  asyncData({ $axios, error }) {},
+}
+</script>
+
+<style lang="scss" scoped></style>
--- pages/articles/index.vue
+++ pages/articles/index.vue
@@ -2,13 +2,15 @@
   <b-container fluid>
     <h1>All Posts</h1>
     <article-table :items="items" />
+    <article-pagination :total="total" />
   </b-container>
 </template>
 
 <script>
 import ArticleTable from '~/components/pages/articles/index/ArticleTable.vue'
+import ArticlePagination from '~/components/pages/articles/index/ArticlePagination.vue'
 export default {
-  components: { ArticleTable },
+  components: { ArticleTable, ArticlePagination },
   async asyncData({ $axios, error }) {
     try {
       const { articles, articlesCount } = await $axios.$get('/articles', {
