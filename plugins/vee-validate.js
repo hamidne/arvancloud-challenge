d9bcbfde6664834f9638f0664324302c3a169296
HamidNE <hamidne@mail.ru>
2020-09-19 15:26:02 +0430

    add localization to validation

--- plugins/vee-validate.js
+++ plugins/vee-validate.js
@@ -1,6 +1,12 @@
 import Vue from 'vue'
-import { extend, ValidationProvider, ValidationObserver } from 'vee-validate'
+import {
+  extend,
+  localize,
+  ValidationProvider,
+  ValidationObserver,
+} from 'vee-validate'
 import * as rules from 'vee-validate/dist/rules'
+import en from 'vee-validate/dist/locale/en.json'
 
 // Import Components
 Vue.component('ValidationProvider', ValidationProvider)
@@ -9,3 +15,5 @@ Vue.component('ValidationObserver', ValidationObserver)
 // Add Rules
 // eslint-disable-next-line import/namespace
 Object.keys(rules).forEach((rule) => extend(rule, rules[rule]))
+
+localize({ en })
