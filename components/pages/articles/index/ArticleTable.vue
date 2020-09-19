d8b5d19f01dbd8b788628c66e6dd269ec71ba301
HamidNE <hamidne@mail.ru>
2020-09-19 08:48:52 +0430

    add article table for project

---
+++ components/pages/articles/index/ArticleTable.vue
@@ -0,0 +1,16 @@
+<template>
+  <b-table striped hover :items="items" />
+</template>
+
+<script>
+export default {
+  props: {
+    items: {
+      type: Array,
+      required: true,
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped></style>
--- pages/articles/index.vue
+++ pages/articles/index.vue
@@ -1,11 +1,14 @@
 <template>
   <b-container fluid>
     <h1>All Posts</h1>
+    <article-table :items="items" />
   </b-container>
 </template>
 
 <script>
+import ArticleTable from '~/components/pages/articles/index/ArticleTable.vue'
 export default {
+  components: { ArticleTable },
   async asyncData({ $axios, error }) {
     try {
       const { articles, articlesCount } = await $axios.$get('/articles')
