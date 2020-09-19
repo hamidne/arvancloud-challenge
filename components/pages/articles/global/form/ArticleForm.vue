51b9cdcae70b9124f305ee795b2a67b6694d0581
HamidNE <hamidne@mail.ru>
2020-09-19 17:56:39 +0430

    add title filed for create new article

--- components/pages/articles/global/form/ArticleForm.vue
+++ components/pages/articles/global/form/ArticleForm.vue
@@ -1,44 +1,44 @@
 <template>
-  <b-form @submit.prevent="submit">
-    <b-row>
-      <b-col md="5">
-        <b-form-group label="Title" label-for="input-title">
-          <b-form-input id="input-title" v-model="innerValue.title" required />
-          <b-form-invalid-feedback>Required field</b-form-invalid-feedback>
-        </b-form-group>
+  <validation-observer ref="observer" v-slot="{ handleSubmit }">
+    <b-form @submit.prevent="handleSubmit(submit)">
+      <b-row>
+        <b-col md="5">
+          <title-input v-model="innerValue.title" />
 
-        <b-form-group label="Description" label-for="input-description">
-          <b-form-input
-            id="input-description"
-            v-model="innerValue.description"
-            required
-          />
-          <b-form-invalid-feedback>Required field</b-form-invalid-feedback>
-        </b-form-group>
+          <b-form-group label="Description" label-for="input-description">
+            <b-form-input
+              id="input-description"
+              v-model="innerValue.description"
+              required
+            />
+            <b-form-invalid-feedback>Required field</b-form-invalid-feedback>
+          </b-form-group>
 
-        <b-form-group label="Body" label-for="input-body">
-          <b-textarea
-            id="input-body"
-            v-model="innerValue.body"
-            rows="10"
-            required
-          />
-          <b-form-invalid-feedback>Required field</b-form-invalid-feedback>
-        </b-form-group>
+          <b-form-group label="Body" label-for="input-body">
+            <b-textarea
+              id="input-body"
+              v-model="innerValue.body"
+              rows="10"
+              required
+            />
+            <b-form-invalid-feedback>Required field</b-form-invalid-feedback>
+          </b-form-group>
 
-        <b-button type="submit" variant="primary">Submit</b-button>
-      </b-col>
-      <b-col>
-        <tags-input v-model="innerValue.tagList" />
-      </b-col>
-    </b-row>
-  </b-form>
+          <b-button type="submit" variant="primary">Submit</b-button>
+        </b-col>
+        <b-col>
+          <tags-input v-model="innerValue.tagList" />
+        </b-col>
+      </b-row>
+    </b-form>
+  </validation-observer>
 </template>
 
 <script>
+import TitleInput from '~/components/pages/articles/global/form/TitleInput.vue'
 import TagsInput from '~/components/pages/articles/global/form/TagsInput.vue'
 export default {
-  components: { TagsInput },
+  components: { TagsInput, TitleInput },
   props: {
     value: {
       type: Object,
