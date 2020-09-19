0bc608805020e3a46b33a301e17999faa839f960
HamidNE <hamidne@mail.ru>
2020-09-19 15:28:39 +0430

    edit for login page validation

--- pages/login/index.vue
+++ pages/login/index.vue
@@ -1,37 +1,32 @@
 <template>
   <div>
     <h1 class="text-center text-muted mb-4">LOGIN</h1>
-    <b-form @submit.prevent="submit">
-      <email-input v-model="form.email" />
-      <b-form-group label="Password" label-for="input-password">
-        <b-form-input
-          id="input-password"
-          v-model="form.password"
-          type="password"
-          :state="false"
-          required
-        />
-        <b-form-invalid-feedback>Required field</b-form-invalid-feedback>
-      </b-form-group>
+    <validation-observer ref="observer" v-slot="{ handleSubmit }">
+      <b-form @submit.prevent="handleSubmit(submit)">
+        <email-input v-model="form.email" />
 
-      <b-button variant="primary" class="mt-2" type="submit" block>
-        Login
-      </b-button>
+        <password-input v-model="form.passwrod" />
 
-      <nuxt-link :to="{ name: 'register' }" class="d-block mt-2 text-dark">
-        <span>Don't have account?</span>
-        <strong>Register Now</strong>
-      </nuxt-link>
-    </b-form>
+        <b-button variant="primary" class="mt-2" type="submit" block>
+          Login
+        </b-button>
+
+        <nuxt-link :to="{ name: 'register' }" class="d-block mt-2 text-dark">
+          <span>Don't have account?</span>
+          <strong>Register Now</strong>
+        </nuxt-link>
+      </b-form>
+    </validation-observer>
   </div>
 </template>
 
 <script>
 import EmailInput from '~/components/pages/login/EmailInput.vue'
+import PasswordInput from '~/components/pages/login/PasswordInput.vue'
 export default {
   layout: 'auth',
-  components: { EmailInput },
-  data: () => ({ form: {} }),
+  components: { EmailInput, PasswordInput },
+  data: () => ({ form: { email: '', password: '' } }),
   methods: {
     submit() {
       this.$auth.login({ data: { user: this.form } })
