bac171937a4de087aa99355af16e95f265696ba9
HamidNE <hamidne@mail.ru>
2020-09-20 23:00:07 +0430

    add test for article slug page

---
+++ pages/articles/edit/_slug.spec.js
@@ -0,0 +1,50 @@
+import { shallowMount } from '@vue/test-utils'
+import axios from 'axios'
+import slug from './_slug.vue'
+
+/* #region  Test setup */
+const article = {
+  title: 'TITLE',
+  description: 'DESCRIPTION',
+  body: 'BODY',
+  tagList: 'TAGLIST',
+}
+const push = jest.fn()
+const toast = jest.fn()
+axios.$put = jest.fn().mockResolvedValue()
+
+const factory = () => {
+  return shallowMount(slug, {
+    data: () => ({ form: article }),
+    mocks: {
+      $axios: axios,
+      $router: { push },
+      $bvToast: { toast },
+      $route: { params: { slug: 'SLUG' } },
+    },
+  })
+}
+/* #endregion */
+
+describe('_slug.vue', () => {
+  let wrapper
+  beforeEach(() => (wrapper = factory()))
+
+  test('should mounts properly', () => {
+    expect(wrapper.vm).toBeTruthy()
+  })
+
+  test('should called submit correctly', async () => {
+    wrapper.vm.submit()
+    expect(axios.$put).toHaveBeenCalledWith('/articles/SLUG', { article })
+
+    await wrapper.vm.$nextTick()
+
+    expect(push).toHaveBeenCalledWith({ name: 'articles' })
+    expect(toast).toHaveBeenCalledWith('Article updated successfuly', {
+      title: 'Well done! ',
+      variant: 'success',
+      solid: true,
+    })
+  })
+})
