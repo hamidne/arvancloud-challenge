d84e75723d70fd46e06fe9be334a65bd19f4e12f
HamidNE <hamidne@mail.ru>
2020-09-20 23:02:36 +0330

    fixed login page tests

--- pages/login/index.spec.js
+++ pages/login/index.spec.js
@@ -2,8 +2,14 @@ import { shallowMount } from '@vue/test-utils'
 import index from './index.vue'
 
 /* #region  Test setup */
+const toast = jest.fn()
+const login = jest
+  .fn()
+  .mockRejectedValue({ response: { data: { errors: [] } } })
 const factory = () => {
-  return shallowMount(index)
+  return shallowMount(index, {
+    mocks: { $auth: { login }, $bvToast: { toast } },
+  })
 }
 /* #endregion */
 
@@ -13,5 +19,20 @@ describe('index.vue', () => {
 
   test('should mounts properly', () => {
     expect(wrapper.vm).toBeTruthy()
+  })
+
+  test('should called submit correctly', async () => {
+    wrapper.vm.submit()
+    expect(login).toHaveBeenCalledWith({
+      data: { user: { email: '', password: '' } },
+    })
+
+    await wrapper.vm.$nextTick()
+
+    expect(toast).toHaveBeenCalledWith('User name and/or Password is invalid', {
+      title: 'Login Failed!',
+      variant: 'danger',
+      solid: true,
+    })
   })
 })
